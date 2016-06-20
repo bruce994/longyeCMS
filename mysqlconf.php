25348f6eaed5e8259fdaca087369cd00b657cd08
bruce994 <26wz.wang@gmail.com>
2016-06-20 17:53:31 +0800

    centos

---
+++ mysqlconf.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+   'DB_HOST' => "115.159.121.115",
+   'DB_NAME' => "longye",
+   'DB_USER' => "longye",
+   'DB_PWD' => "6ANBb6EhCKbBZfnG",
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => "dede_",
+
+);
+?>
