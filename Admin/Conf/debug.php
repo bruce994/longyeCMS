6dcea9bf1e0cc7c42d73acc334bea09db9d1698a
bruce <test11798@gmail.cm>
2016-07-01 17:10:25 +0800

    mac

--- Admin/Conf/debug.php
+++ Admin/Conf/debug.php
@@ -6,6 +6,6 @@ return array(
 	'SHOW_DB_TIMES'=>false,			// 显示数据库查询和写入次数
 	'SHOW_CACHE_TIMES'=>false,		// 显示缓存操作次数
 	'SHOW_USE_MEM'=>false,			// 显示内存开销
-	'SHOW_PAGE_TRACE'=>true			//显示调试信息
+	'SHOW_PAGE_TRACE'=>false			//显示调试信息
 );
 ?>
